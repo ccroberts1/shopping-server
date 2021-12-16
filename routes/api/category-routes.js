eeb863614f63b0808842ecf3a3a65f88d6075ea9
Caitlin Roberts <caitlin.coy.roberts@gmail.com>
2021-12-15 19:33:03 -0700

    added routes for updating and deleting categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -43,11 +43,32 @@ router.post("/", (req, res) => {
 });
 
 router.put("/:id", (req, res) => {
-  // update a category by its `id` value
+  Category.update(
+    {
+      category_name: req.body.category_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((updatedCat) => {
+      res.json(updatedCat);
+    })
+    .catch((err) => res.json(err));
 });
 
 router.delete("/:id", (req, res) => {
-  // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((deletedCat) => {
+      res.json(deletedCat);
+    })
+    .catch((err) => res.json(err));
 });
 
 module.exports = router;
